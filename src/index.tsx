d70ab2e5374b9dac1ea4a243222728cd93c47d7a
teabyii <teabyii@gmail.com>
2019-11-29 17:19:07 +0800

    fix: export merge helper

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ export default Provider;
 export { createContext } from './context';
 export { connect } from './connect';
 export { applyGlobalMiddleware, cancelGlobalMiddleware } from './global';
+export { merge } from './helper';
 
 // devtools support
 import initDevtools from './devtools';
